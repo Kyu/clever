2806b2e86134b5bc1a850245dfbf5826756da121
Kyu <rocketword@gmail.com>
2017-04-10 16:48:17 -0400

    made methods async friendly

--- clever.py
+++ clever.py
@@ -1,23 +1,27 @@
-import requests
+import aiohttp
 import json
+import asyncio
 
 
 class CleverBot(object):
-    def __init__(self, user, key, nick=None):
+    def __init__(self, user, key, nick=None, loop=None):
+        self.loop = asyncio.get_event_loop() if loop is None else loop
         self.user = user
         self.key = key
         self.nick = nick
+        self.loop.run_until_complete(self._session())
+        self.ask = self.query
 
+    async def _session(self):
         body = {
-            'user': user,
-            'key': key,
-            'nick': nick
+            'user': self.user,
+            'key': self.key,
+            'nick': self.nick
         }
+        self.session = aiohttp.ClientSession(loop=self.loop)
+        await self.session.post('https://cleverbot.io/1.0/create', data=body)
 
-        requests.post('https://cleverbot.io/1.0/create', json=body)
-
-
-    def query(self, text):
+    async def query(self, text):
         body = {
             'user': self.user,
             'key': self.key,
@@ -25,8 +29,8 @@ class CleverBot(object):
             'text': text
         }
 
-        r = requests.post('https://cleverbot.io/1.0/ask', json=body)
-        r = json.loads(r.text)
+        r = await self.session.post('https://cleverbot.io/1.0/ask', data=body)
+        r = json.loads(await r.text())
 
         if r['status'] == 'success':
             return r['response']
